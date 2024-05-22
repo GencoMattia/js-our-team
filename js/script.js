6c6f5c921e86f8fadce3ea6bd8dcb92a4ed684bf
GencoMattia <gencomattia@hotmail.it>
2024-05-22 16:27:34 +0200

    printed all info on the dom

--- js/script.js
+++ js/script.js
@@ -35,13 +35,20 @@ for(let i = 0; i < ourTeam.length; i++){
     const teamMember = ourTeam[i];
     const teamContainerEl = document.querySelector(".team-cards");
     const teamMemberEl = document.createElement("article");
+    
     teamContainerEl.appendChild(teamMemberEl);
+
     const memberNameEl = document.createElement("h3");
     const memberRoleEl = document.createElement("p");
-    const memberPictureEl = document.createElement("img");
+    const memberPictureEl = document.createElement("p");
+
     teamMemberEl.appendChild(memberNameEl);
     teamMemberEl.appendChild(memberRoleEl);
     teamMemberEl.appendChild(memberPictureEl);
+
+    memberNameEl.append(teamMember.name);
+    memberRoleEl.append(teamMember.role);
+    memberPictureEl.append(teamMember.photo);
 
     for (let key in teamMember) {
         console.log(key, teamMember[key]);
